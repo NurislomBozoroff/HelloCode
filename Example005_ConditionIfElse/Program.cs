e22ed08f4b21d766d317905e115184fd44fc5fdf
Nurislom <nurislombozorov@outlook.com>
2022-10-19 15:09:07 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя ползователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура ето же Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
